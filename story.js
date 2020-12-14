65d3ba4e8ab8b32d6057c0315536cca9327d582e
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2020-12-15 05:02:43 +0900

    JS : json 데이터 Story에 삽입

---
+++ story.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function loaddata() {
+    return fetch('data.json')
+        .then(response => response.json())
+        .then(json => json.items);
+}
+
+
+const list = document.querySelector('.story__list');
+const itemLength = 80;
+let listWidth = 80;
+function displayItem(items) {
+    console.log(items)
+    items.forEach(item => {
+        if(item.follow == 'true'){
+            listWidth += itemLength;
+    let listItem = document.createElement('li');
+    listItem.innerHTML = `
+    <li class="story__item">
+                <div class="story__item-profile">
+                  <img src="${item.image}" alt="" />
+                </div>
+                <span>${item.name}</span>
+              </li>
+    `;
+    list.appendChild(listItem);
+    }
+});
+    list.style.width = `${listWidth + 40}px`
+}
+
+loaddata()
+.then(items => displayItem(items));
