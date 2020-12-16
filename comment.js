25ddeb9d8d8316876271e37d7003dfb6f98690de
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2020-12-17 03:19:42 +0900

    JS : LocalStorage에 댓글 저장

--- comment.js
+++ comment.js
@@ -1,11 +1,59 @@
 'use strict';
 
 const comment = document.querySelector('.input-comment');
+let commentItems = [];
+const newItems = commentItems;
 
 comment.addEventListener('keyup', (event) => {
     if(event.key === 'Enter') {
-        console.log('input!!')
-        console.log(comment.value)
+        let currentValue = comment.value;
+        createElement(currentValue);
         comment.value = '';
     }
 });
+
+
+function saveElement() {
+    localStorage.setItem('commentItems', JSON.stringify(newItems));
+}
+
+const commentList = document.querySelector('.feed__comment-list');
+function createElement(value) {
+    const newId = commentItems.length + 1;
+    let commentItem = document.createElement('li');
+    commentItem.classList.add('feed__comment-item');
+    commentItem.innerHTML = `
+    <div class="feed__comment-left">
+      <div class="comment-left__name">
+        <span>waitwait0301</span>
+      </div>
+      <div class="comment-left__description">
+        <span>${value}</span>
+      </div>
+    </div>
+    <div class="feed__comment-heart">
+      <i class="far fa-heart" style="font-size: 12px"></i>
+    </div>
+    `
+    commentList.appendChild(commentItem);
+    
+    const ItemObj = {
+        value : value, 
+        id : newId,
+    };
+    commentItems.push(ItemObj);
+    saveElement(value);
+}
+
+function loadData() {
+    let output = localStorage.getItem('commentItems');
+    let lodedItems = JSON.parse(output);
+    if(lodedItems !== null) {
+        lodedItems.forEach(loadItem => {
+            createElement(loadItem.value);
+        });
+        
+    }
+}
+
+loadData();
