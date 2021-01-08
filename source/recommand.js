01ff1399182f9ab835b8d3ca3937334851dc4444
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2021-01-08 23:48:28 +0900

    JS : 팔로우 버튼 클릭시 팔로잉으로 변경

--- source/recommand.js
+++ source/recommand.js
@@ -8,10 +8,12 @@ function loaddata() {
 
 const recommandList = document.querySelector('.recommand__list');
 function displayRecommandItem(item) {
-
+  let i = 0;
     item.forEach(item => {
         if(item.follow == 'true'){
             return;
+        } else {
+          i++;
         }
         let recommandItem = document.createElement('li');
         recommandItem.classList.add('recommand__item');
@@ -27,10 +29,38 @@ function displayRecommandItem(item) {
                   </div>
                 </div>
               </div>
-              <div class="recommand__right"><span>팔로우</span></div>
+              <div class="recommand__right" data-num ="${i}"><span class="follow">팔로우</span></div>
         `
         recommandList.appendChild(recommandItem);
     });
+}
+
+
+
+recommandList.addEventListener('click', (e) => {
+  const follow = document.querySelectorAll('.recommand__right');
+  const target = e.target;
+  
+  if(target.parentNode.classList[0] == 'recommand__right') {
+    switch(target.parentNode.dataset.num) {
+      case '1': toggleFollow(0, follow);
+      break;
+      case '2': toggleFollow(1, follow);
+      break;
+      case '3': toggleFollow(2, follow);
+      break;
+      case '4': toggleFollow(3, follow);
+      break;  
+    }
+  }
+})
+
+function toggleFollow(data, follow) {
+  if(follow[data].childNodes[0].classList[0] =='follow') {
+    follow[data].innerHTML = `<span class="following">팔로잉</span>`
+  } else {
+    follow[data].innerHTML = `<span class="follow">팔로우</span>`
+  }
 }
 
 loaddata()
